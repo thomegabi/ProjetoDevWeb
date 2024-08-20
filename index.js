38eadb3ace18124f68ff9309b4b72b32b0fcda12
My name <test@example.com>
2024-08-19 21:42:52 -0300

    Implementa index.js e dependencias

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const  { createServer } = require('http');
 const express = require ('express');
 
 const dotenv = require('dotenv');
-
-dotenv.config();  // procura o arquivo .env e le
+const bodyParser = require('body-parser');
 
 let app = express();
 
-const http = createServer(app);
+dotenv.config();  // procura o arquivo .env e le
+
+app.use(bodyParser.json());
+
+//app.use(express.json()); // funciona dos 2 jeitos
 
 process.on('SIGINT', () => http.close((error) => {
     if(error){
-        console.log(`${error.name}: ${error.message}`);
+        console.log(`${error.name}: ${error.message}`)
     }
 
-    process.exit(error ? 1 : 0);
-}));
+    process.exit(error ? 1:0);
+}))
+
+
+
+const http = createServer(app);
+
+app.get('/', (req, res) => res.send('<h1>Resposta</h1>'));
+
+app.post('/', function (req, res){
+    const name = req.body.name;
+    res.send(`Olá ${name}`);
+})
 
 http.listen(8989, () => console.log('Listening to port 8989'));
